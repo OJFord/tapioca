65f83e6160385ee1d6587e111d251ed73d76f733
Ollie Ford <me@ojford.com>
2017-05-30 14:46:35 +0100

    Avoid needless unit structs when no query params
    Previously a struct e.g. `uber::product::PatchQueryParams` would be
    created regardless of whether the schema specified any query parameters
    to use as fields.
    
    This commit does not create such a struct unless it is used.

--- tapioca-derive/src/infer/method.rs
+++ tapioca-derive/src/infer/method.rs
@@ -31,43 +31,34 @@ fn query_param_struct_ident(method: &str) -> Ident {
 
 pub(super) fn infer_v3(method: &str, schema: &Yaml) -> TokensResult {
     let method_fn = fn_ident(&method);
-    let query_param_st = query_param_struct_ident(&method);
 
-    let query_params: Vec<(Tokens, Tokens)>;
-    let signature: Tokens;
+    let mut query_param_setter_bb = Tokens::new();
+    let mut query_param_st = Tokens::new();
+    let mut signature = Tokens::new();
+
     if let Some(parameters) = schema["parameters"].as_vec() {
-        query_params = parameters.iter()
+        let query_param_st_ident = query_param_struct_ident(&method);
+        let query_params: Vec<(Tokens, Tokens)> = parameters.iter()
             .filter(|p| p["in"] == Yaml::from_str("query"))
             .map(|p| parameter::infer_v3(p).unwrap())
             .collect();
-        signature = quote!{ query_parameters: &#query_param_st };
-    } else {
-        query_params = Vec::new();
-        signature = quote!{};
-    }
 
-    let (query_param_fields, query_param_types): (Vec<Tokens>, Vec<Tokens>) = (
-        query_params.iter().cloned().map(|(p, _)| p).collect(),
-        query_params.iter().cloned().map(|(_, t)| t).collect(),
-    );
-    let query_param_strings: Vec<Tokens> = query_param_fields.iter().cloned()
-        .map(|p| quote!{ "#p" }).collect();
-    let query_param_accessors: Vec<Tokens> = query_param_fields.iter().cloned()
-        .map(|f| quote!{ query_parameters.#f }).collect();
+        let (query_param_fields, query_param_types): (Vec<Tokens>, Vec<Tokens>) = (
+            query_params.iter().cloned().map(|(p, _)| p).collect(),
+            query_params.iter().cloned().map(|(_, t)| t).collect(),
+        );
+        let query_param_accessors: Vec<Tokens> = query_param_fields.iter().cloned()
+            .map(|f| quote!{ query_parameters.#f }).collect();
+        let query_param_strings: Vec<Tokens> = query_param_fields.iter().cloned()
+            .map(|p| quote!{ "#p" }).collect();
 
-    Ok(quote! {
-        type Response = ();
-
-        #[allow(dead_code)]
-        pub(in super::super) struct #query_param_st {
-            #(pub(in super::super) #query_param_fields: #query_param_types),*
-        }
-
-        #[allow(dead_code)]
-        pub(in super::super) fn #method_fn(#signature) -> Response {
-            let mut url = tapioca::Url::parse(self::API_URL)
-                .expect("Malformed server URL or path.");
-            url.set_path(self::API_PATH);
+        query_param_st = quote! {
+            #[allow(dead_code)]
+            pub(in super::super) struct #query_param_st_ident {
+                #(pub(in super::super) #query_param_fields: #query_param_types),*
+            }
+        };
+        query_param_setter_bb = quote! {
             url.query_pairs_mut()
                 .clear()
                 #(.append_pair(
@@ -75,6 +66,21 @@ pub(super) fn infer_v3(method: &str, schema: &Yaml) -> TokensResult {
                     #query_param_accessors.to_string().as_str()
                 ))*
                 ;
+        };
+        signature = quote!{ query_parameters: &#query_param_st_ident };
+    }
+
+    Ok(quote! {
+        type Response = ();
+
+        #query_param_st
+
+        #[allow(dead_code)]
+        pub(in super::super) fn #method_fn(#signature) -> Response {
+            let mut url = tapioca::Url::parse(self::API_URL)
+                .expect("Malformed server URL or path.");
+            url.set_path(self::API_PATH);
+            #query_param_setter_bb
 
             let client = tapioca::Client::new().unwrap();
             client.#method_fn(url).send();
