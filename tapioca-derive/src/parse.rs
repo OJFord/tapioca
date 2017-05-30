fb63ee6e6f425e4555e223fe2d3cc7799e4b28c4
Ollie Ford <me@ojford.com>
2017-05-30 17:39:47 +0100

    Hide schema cache dir and fix double .yml extension

--- tapioca-derive/src/parse.rs
+++ tapioca-derive/src/parse.rs
@@ -4,12 +4,14 @@ use std::io::prelude::*;
 use ::reqwest;
 use ::yaml_rust::{Yaml, YamlLoader};
 
+const CACHE_DIR: &'static str = ".tapioca-cache";
+
 type Schema = Yaml;
 type SchemaResult = Result<Schema, Box<Error + Send + Sync>>;
 
-fn local_copy_location(schema_fname: &String) -> String {
-    fs::create_dir_all("tapioca-schemata").unwrap();
-    format!("tapioca-schemata/{}.yml", schema_fname)
+fn cache_path(schema_fname: &String) -> String {
+    fs::create_dir_all(&CACHE_DIR).unwrap();
+    format!("{}/{}", &CACHE_DIR, &schema_fname)
 }
 
 fn parse_first_doc(buf: &str) -> SchemaResult {
@@ -22,7 +24,7 @@ fn parse_first_doc(buf: &str) -> SchemaResult {
 }
 
 pub(super) fn parse_schema(schema_fname: &String) -> SchemaResult {
-    let mut file = fs::File::open(local_copy_location(&schema_fname))?;
+    let mut file = fs::File::open(cache_path(&schema_fname))?;
     let mut buf = String::new();
 
     file.read_to_string(&mut buf)?;
@@ -30,7 +32,7 @@ pub(super) fn parse_schema(schema_fname: &String) -> SchemaResult {
 }
 
 pub(super) fn fetch_schema(schema_fname: &String, schema_url: &str) -> SchemaResult {
-    let mut file = fs::File::create(local_copy_location(&schema_fname))?;
+    let mut file = fs::File::create(cache_path(&schema_fname))?;
     let mut buf = String::new();
 
     let mut resp = reqwest::get(schema_url)?;
