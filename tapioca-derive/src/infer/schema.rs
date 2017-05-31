0dc75fb8931a0d5d87dbf4825fcf7f0595874f74
Ollie Ford <me@ojford.com>
2017-05-31 22:03:16 +0100

    Support $ref type schemata that $ref another

--- tapioca-derive/src/infer/schema.rs
+++ tapioca-derive/src/infer/schema.rs
@@ -6,6 +6,29 @@ use infer::parameter;
 use infer::path;
 use infer::TokensResult;
 
+type FieldsAndSupportingTypes = (Vec<Tokens>, Vec<Tokens>);
+
+fn infer_ref(schema: &Yaml) -> FieldsAndSupportingTypes {
+    let mut fields: Vec<Tokens> = Vec::new();
+    let mut additional_types: Vec<Tokens> = Vec::new();
+
+    for (field_name, schema) in schema["properties"].as_hash()
+        .expect("Properties must be a map.")
+    {
+        let field_name = field_name.as_str()
+            .expect("Property must be a string.");
+        let field_ident = Ident::new(field_name);
+        let (field_type, additional_type) = parameter::infer_type(&schema).unwrap();
+
+        fields.push(quote!{ #field_ident: #field_type });
+        if let Some(additional_type) = additional_type {
+            additional_types.push(additional_type);
+        }
+    }
+
+    (fields, additional_types)
+}
+
 pub(super) fn infer_v3(schema: &Yaml) -> TokensResult {
     let paths = schema["paths"].clone();
     let path_impls: Vec<Tokens> = paths.as_hash()
@@ -26,20 +49,11 @@ pub(super) fn infer_v3(schema: &Yaml) -> TokensResult {
         let schema_ref_name = schema_ref.as_str()
             .expect("$ref name must be a string.");
         let ident = Ident::new(schema_ref_name);
-        let mut fields: Vec<Tokens> = Vec::new();
-
-        for (field_name, schema) in schema["properties"].as_hash()
-            .expect("Properties must be a map.")
-        {
-            let field_name = field_name.as_str()
-                .expect("Property must be a string.");
-            let field_ident = Ident::new(field_name);
-            let field_type = parameter::infer_type(&schema)?;
-
-            fields.push(quote!{ #field_ident: #field_type });
-        }
+        let (fields, additional_types) = infer_ref(schema);
 
         schema_ref_struct_defs.push(quote! {
+            #(#additional_types)*
+
             struct #ident {
                 #(#fields),*
             }
