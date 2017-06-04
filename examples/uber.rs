ee4831b8b34b6514a2ddeb55c4d01a0a6dc6b21d
Ollie Ford <me@ojford.com>
2017-06-04 21:08:23 +0100

    Tidy up example

--- examples/uber.rs
+++ examples/uber.rs
@@ -8,27 +8,27 @@ extern crate tapioca;
 infer_api!(uber, "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/OpenAPI.next/examples/v3.0/uber.yaml");
 
 fn main() {
-    use uber::products;
+    use uber::products::get::{QueryParams, OkBody, ErrBody};
 
-    let query_params = products::get::QueryParams {
+    let query_params = QueryParams {
         latitude: 10.3,
         longitude: 237.8,
     };
 
-    match products::get(query_params) {
+    match uber::products::get(query_params) {
         Ok(result) => match result.body() {
-            products::get::OkBody::Status200(body) => {
+            OkBody::Status200(body) => {
                 let list = body.products.unwrap_or(vec![]);
                 println!("First product: {:?}", list[0]);
             },
         },
         Err(result) => match result.body() {
-            products::get::ErrBody::Status520(body) => {
-                println!("Error message: {:?}", body.message);
-            },
-            products::get::ErrBody::NetworkFailure() => {
-                println!("Request failed!");
-            },
+            ErrBody::Status520(body) => println!(
+                "Error message: {:?}",
+                body.message
+                    .unwrap_or(String::from("[No message]"))
+            ),
+            ErrBody::NetworkFailure() => println!("Request failed!"),
         },
     }
 }
