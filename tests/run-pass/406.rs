d3702d54d9ce059496bf46fffe8f08a230b0a023
Ollie Ford <me@ojford.com>
2017-06-12 14:43:41 +0100

    Add test for correct Accept header

---
+++ tests/run-pass/406.rs
@@ -0,0 +1,20 @@
+#![feature(associated_consts)]
+#![feature(use_extern_macros)]
+#![allow(plugin_as_library)]
+
+#[macro_use]
+extern crate tapioca;
+
+infer_api!(httpbin, "https://raw.githubusercontent.com/OJFord/tapioca/master/tests/schemata/httpbin.yml");
+
+fn main() {
+    match httpbin::anything::get() {
+        Ok(response) => match response.body() {
+            httpbin::anything::get::OkBody::Status200(body) => assert!(
+                body.headers.Accept.contains("application/json")
+            ),
+            _ => assert!(false),
+        },
+        Err(_) => assert!(false),
+    }
+}
