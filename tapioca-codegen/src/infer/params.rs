1186b1659874ea0cce98d864c6b96091dc115b35
Ollie Ford <me@ojford.com>
2017-06-16 01:16:52 +0100

    Wrap path params in a hidden type
    This is to prevent automatic implementation of the Clone trait, and
    probably sufficient to prevent client libraries implementing it too.

--- tapioca-codegen/src/infer/params.rs
+++ tapioca-codegen/src/infer/params.rs
@@ -6,11 +6,10 @@ use ::yaml_rust::Yaml;
 use infer::datatype;
 use infer::StructBoundArgImpl;
 
-fn ident(param: &str) -> Ident {
-    Ident::new(param.to_snake_case())
-}
-
 pub(super) fn infer_v3(method: &str, schema: &Yaml) -> StructBoundArgImpl {
+    let method_mod = Ident::new(method.to_snake_case());
+    let method = method.to_uppercase();
+
     let mut idents: Vec<Ident> = Vec::new();
     let mut types: Vec<Tokens> = Vec::new();
     let mut supporting_types: Vec<Tokens> = Vec::new();
@@ -21,8 +20,20 @@ pub(super) fn infer_v3(method: &str, schema: &Yaml) -> StructBoundArgImpl {
             .expect("Parameter name must be a string.");
         let (param_type, maybe_at) = datatype::infer_v3(&schema["schema"])?;
 
-        idents.push(ident(name));
-        types.push(quote!{ ::tapioca::datatype::Required<#param_type> });
+        let struct_ident = Ident::new(format!("__ResourceId_{}", name));
+        supporting_types.push(quote! {
+            #[allow(non_camel_case_types)]
+            pub struct #struct_ident(::tapioca::datatype::Required<#param_type>);
+
+            impl ToString for #struct_ident {
+                fn to_string(&self) -> String {
+                    self.0.to_string()
+                }
+            }
+        });
+
+        idents.push(Ident::new(name));
+        types.push(quote!{ #method_mod::#struct_ident });
         placeholders.push(format!("{{{}}}", name));
 
         if let Some(supporting_type) = maybe_at {
@@ -32,8 +43,8 @@ pub(super) fn infer_v3(method: &str, schema: &Yaml) -> StructBoundArgImpl {
 
     let params = idents.clone();
 
-    let endpoint_id_arg = match (method.as_ref(), idents.pop(), types.pop()) {
-        ("delete", Some(endp_ident), Some(endp_type))
+    let endpoint_id_arg = match (method.as_str(), idents.pop(), types.pop()) {
+        ("DELETE", Some(endp_ident), Some(endp_type))
             // The resource ID value is moved here, to avoid its reuse
             // !FIXME: this assumes that the DELETE request succeeds
             => quote!(#endp_ident: #endp_type),
--- tests/compile-fail/410.rs
+++ tests/compile-fail/410.rs
@@ -6,7 +6,7 @@ tapioca_testutil::infer_test_api!(httpbin);
 use httpbin::status__code_;
 
 fn main() {
-    let dummy_created_id: i32 = 200;
+    let dummy_created_id = status__code_::get::__ResourceId_code(200);
 
     status__code_::get(&dummy_created_id);
     status__code_::delete(dummy_created_id); //~ moved here
