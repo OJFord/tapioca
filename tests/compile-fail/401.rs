fcda679af214111e48f519f2035ad27c18b28daf
Ollie Ford <me@ojford.com>
2017-06-18 06:00:39 +0100

    Test 401 with HTTP Basic auth

--- tests/compile-fail/401.rs
+++ tests/compile-fail/401.rs
@@ -4,8 +4,11 @@ extern crate tapioca_testutil;
 tapioca_testutil::infer_test_api!(httpbin);
 
 fn main() {
-    let auth = httpbin::ServerAuth::new();
+    let server_auth = httpbin::ServerAuth::new();
+    let op_auth = httpbin::basic_auth::OpAuth::HttpBasic::from(("".into(), "".into()));
+    let query = httpbin::basic_auth::get::QueryParams { user };
 
-    httpbin::anything::get(); //~ takes 1 parameter but 0 parameters were supplied
-    httpbin::anything::get(auth); // OK
+    httpbin::basic_auth::get(&query); //~ takes 2 parameters but 1 parameter was supplied
+    httpbin::basic_auth::get(&query, server_auth); //~ mismatched types
+    httpbin::basic_auth::get(&query, op_auth); // [OK]
 }
--- tests/run-pass/401.rs
+++ tests/run-pass/401.rs
@@ -4,10 +4,26 @@ extern crate tapioca_testutil;
 tapioca_testutil::infer_test_api!(httpbin);
 
 fn main() {
-    let auth = httpbin::ServerAuth::new();
+    let user = String::from("Foo");
+    let mut auth = httpbin::basic_auth::HttpBasic::from((user, "wrong".into()));
+    let query = httpbin::basic_auth::get::QueryParams { user };
 
-    match httpbin::anything::get(auth) {
-        Ok(response) => assert!(true),
-        Err(response) => assert!(false),
+    match httpbin::basic_auth::get(&query, auth) {
+        Ok(_) => assert!(false),
+        Err(response) => match response.body() {
+            httpbin::basic_auth::get::ErrBody::Status401() => assert!(true),
+            _ => assert!(false),
+        },
+    }
+
+    auth.password = "hunter2";
+    match httpbin::basic_auth::get(&query, auth) {
+        Ok(response) => match response.body() {
+            httpbin::basic_auth::get::OkBody::Status200(body) => {
+                assert!(body.authenticated);
+                assert_eq!(body.user, user);
+            }
+        },
+        Err(_) => assert!(false),
     }
 }
